a2895fe6f1a5fde20a0d9acd3e16c5125f28e72d
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-07-23 10:14:47 +0200

    feat: adding footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   );
 }
